2f002f1c74abd7e75b7fee266f19ffe3ad19ca11
Theodor Müller <info@trilliarden.net>
2025-02-18 10:18:24 +0100

    Add color schemes

--- init.lua
+++ init.lua
@@ -230,6 +230,10 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  "folke/tokyonight.nvim",
+  "catppuccin/nvim",
+  'projekt0n/github-nvim-theme',
+  
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -982,3 +986,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.cmd[[colorscheme tokyonight-day]]
